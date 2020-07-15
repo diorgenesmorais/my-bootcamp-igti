3dffd9a3ba8e52a609d5d8463f024f6be779bb7e
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-15 01:53:10 -0300

    I also simplified green and blue

--- module1/practical/js/main.js
+++ module1/practical/js/main.js
@@ -6,15 +6,7 @@ window.addEventListener('load', function(){
     const greenResult = document.querySelector('#greenResult');
     const blueResult = document.querySelector('#blueResult');
     const painting = document.querySelector('.painting');
-    
-    function greenValue() {
-        return Math.round(greenRange.value * 2.55);
-    }
-    
-    function blueValue() {
-        return Math.round(blueRange.value * 2.55);
-    }
-    
+
     redRange.addEventListener('input', showColor);
     
     greenRange.addEventListener('input', showColor);
@@ -23,9 +15,9 @@ window.addEventListener('load', function(){
     
     function showColor() {
         redResult.value = redRange.value;
-        greenResult.value = greenValue();
-        blueResult.value = blueValue();
-        painting.style.backgroundColor = `rgb(${redResult.valueAsNumber}, ${greenValue()}, ${blueValue()})`;
+        greenResult.value = greenRange.value
+        blueResult.value = blueRange.value
+        painting.style.backgroundColor = `rgb(${redResult.valueAsNumber}, ${greenRange.valueAsNumber}, ${blueRange.valueAsNumber})`;
     }
     
     showColor();
