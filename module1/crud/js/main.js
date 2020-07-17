153748634505a30a133cf7caee7204534b90cdd1
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-17 06:50:33 -0300

    add names

--- module1/crud/js/main.js
+++ module1/crud/js/main.js
@@ -1,11 +1,31 @@
 (function() {
     document.querySelector('form').addEventListener('submit', (event) => event.preventDefault());
 
+    const namesList = [];
     const inputName = document.querySelector('#nome');
+    inputName.addEventListener('keyup', handleKey);
+
+    const acceptedActions = {
+        Enter(name) {
+            addNames(name);
+        }
+    }
 
     function setFocus() {
         inputName.focus();
     }
 
     setFocus();
+
+    function handleKey(event) {
+        const action = acceptedActions[event.key];
+        if (action) {
+            action(event.target.value);
+        }
+    }
+
+    function addNames(name) {
+        namesList.push(name);
+        console.log(namesList);
+    }
 })();
