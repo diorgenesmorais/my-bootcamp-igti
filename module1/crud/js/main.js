5d21aaeba4a5e3b235cf01574c9b080bd471ddc2
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-17 08:53:58 -0300

    added render

--- module1/crud/js/main.js
+++ module1/crud/js/main.js
@@ -4,6 +4,7 @@
     const namesList = [];
     const inputName = document.querySelector('#nome');
     inputName.addEventListener('keyup', handleKey);
+    const div = document.querySelector('.js-list');
 
     const acceptedActions = {
         Enter(name) {
@@ -33,6 +34,31 @@
             namesList.push(name);
             clearInput();
             console.log(namesList);
+            render();
         }
+    }
+
+    function render() {
+        div.innerHTML = '';
+
+        const ul = document.createElement('ul');
+
+        function createDeleteButton(index) {
+            let button = document.createElement('button');
+            button.textContent = 'x';
+            button.classList.add('deleteButton');
+
+            return button;
+        }
+
+        namesList.forEach((name, index) => {
+            const li = document.createElement('li');
+            const span = document.createElement('span');
+            span.textContent = name;
+            li.appendChild(createDeleteButton(index));
+            li.appendChild(span);
+            ul.appendChild(li);
+        });
+        div.appendChild(ul);
     }
 })();
--- module1/crud/requisitos.md
+++ module1/crud/requisitos.md
@@ -4,7 +4,7 @@
 * [ok] Deve dar entrada no nome apenas teclando enter
 * [ok] Deve apagar o nome do campo após inserir
 * [ok] Não deve inserir espaço vazio
-* [ ] Deve inserir os nomes em uma lista e listar abaixo
+* [ok] Deve inserir os nomes em uma lista e listar abaixo
 * [ ] Deve entrar e modo de edição ao clicar em cima do nome
 * [ ] Após editar o nome e teclar enter ele deve ser exibido na lista
 * [ ] Deve apagar o nome da lista
