bb44414620d3a2c9848af4c1ba35f82771a30fb4
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-22 19:47:44 -0300

    add podcasts file - podcasts list

---
+++ module1/radio-podcast/js/podcasts.js
@@ -0,0 +1,26 @@
+let realPodcast = [
+    {
+        id: '88.5',
+        title: 'Sintax',
+        description: '[Inglês] Desenvolvimento Javascript em geral. Apresentado por Wes Bos e Scott Tolinksi',
+        img: '1.png'
+    },
+    {
+        id: '90.1',
+        title: 'FalaDev',
+        description: 'Desenvolvimento Javascript em geral. Apresentado pelo pessoal da Rocktseat',
+        img: '2.png'
+    },
+    {
+        id: '92.3',
+        title: 'Lambda3',
+        description: 'Desenvolvimento e diversidade. Apresentado pelo pessoal da Lambda3',
+        img: '3.png'
+    },
+    {
+        id: '93.9',
+        title: 'DevNaEstrada',
+        description: 'Desenvolvimento em geral e histórias de carreira de desenvolvedores',
+        img: '4.png'
+    }
+]
