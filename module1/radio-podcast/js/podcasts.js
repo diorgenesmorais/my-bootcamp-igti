8ebd72a4d921dda06db28dc8f998b9d73b9670f3
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-22 23:34:50 -0300

    i refactored the image names

--- module1/radio-podcast/js/podcasts.js
+++ module1/radio-podcast/js/podcasts.js
@@ -3,24 +3,24 @@ let realPodcast = [
         id: '88.5',
         title: 'Sintax',
         description: '[Inglês] Desenvolvimento Javascript em geral. Apresentado por Wes Bos e Scott Tolinksi',
-        img: '1.png'
+        img: 'syntax.png'
     },
     {
         id: '90.1',
         title: 'FalaDev',
         description: 'Desenvolvimento Javascript em geral. Apresentado pelo pessoal da Rocktseat',
-        img: '2.png'
+        img: 'faladev.jpg'
     },
     {
         id: '92.3',
         title: 'Lambda3',
         description: 'Desenvolvimento e diversidade. Apresentado pelo pessoal da Lambda3',
-        img: '3.png'
+        img: 'lambda3.jpeg'
     },
     {
         id: '93.9',
         title: 'DevNaEstrada',
         description: 'Desenvolvimento em geral e histórias de carreira de desenvolvedores',
-        img: '4.png'
+        img: 'devnaestrada.png'
     }
 ]
