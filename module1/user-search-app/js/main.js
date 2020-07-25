0403cc94c011d0123432597ca1dbbe76bb5ae709
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-25 19:26:40 -0300

    catch event submit

--- module1/user-search-app/js/main.js
+++ module1/user-search-app/js/main.js
@@ -1,6 +1,12 @@
 "use strict";
 (function() {
+    document.querySelector('form').addEventListener('submit', event => {
+        event.preventDefault();
+        handleInput();
+    });
     let peoples = [];
+    const inputSearch = document.querySelector('#inputSearch');
+
     async function init() {
         try {
             const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
@@ -22,4 +28,9 @@
     }
     init();
 
+    function handleInput() {
+        if (inputSearch.value.trim().length !== 0) {
+            console.log(inputSearch.value);
+        }
+    }
 })();
