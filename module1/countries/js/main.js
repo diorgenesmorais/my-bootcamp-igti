cfff3125877bc3d6f7e15b33e70ee954678c9603
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-26 18:06:01 -0300

    create application states variables

---
+++ module1/countries/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+(function() {
+    const countCountCountries = document.querySelector('#countCountCountries');
+    const totalPopulation = document.querySelector('#totalPopulation');
+    const tabCountries = document.querySelector('#tabCountries');
+
+    const countFavorites = document.querySelector('#countFavorites');
+    const totalSelection = document.querySelector('#totalSelection');
+    const tabFavorites = document.querySelector('#tabFavorites');
+
+})();
--- module1/countries/screenplay.md
+++ module1/countries/screenplay.md
@@ -1,6 +1,6 @@
 Screenplay
 
-()  Criar variáveis de estado da aplicação
+(√) Criar variáveis de estado da aplicação
 ()  Implementar fetchCountries() com async/await com busca em 'https://restcountries.eu/rest/v2/all'
 ()  Implementar render() com invocação a funções menores:
     renderCountryList, renderFavorites, renderSummary e handleCountryButtons.
