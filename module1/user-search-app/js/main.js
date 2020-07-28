8f5e52db3de8ad119c1d0fdfcf22b3609544b17e
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-07-28 07:21:18 -0300

    experiment: user search app

--- module1/user-search-app/js/main.js
+++ module1/user-search-app/js/main.js
@@ -16,8 +16,7 @@
         try {
             const response = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
             const data = await response.json();
-            const list = [ ...data.results ];
-            peoples = list.map(people => {
+            peoples = data.results.map(people => {
                 const { name, picture, dob, gender } = people;
                 return {
                     name: `${name.first} ${name.last}`,
