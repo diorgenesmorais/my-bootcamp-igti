de8aece9631721c743ced573cbb619cf643b7350
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-02 11:06:59 -0300

    feat: add readLine

--- module2/desafio2/index.js
+++ module2/desafio2/index.js
@@ -1,4 +1,15 @@
 "use stric";
+import readLine from "readline";
 import em from "./src/generateSet.js";
 
-em.emit('createGroup');
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Digite qual evento quer executar:', event => {
+    if (event) {
+        em.emit(event);
+    }
+    rl.close();
+});
