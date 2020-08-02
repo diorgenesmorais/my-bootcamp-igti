84fa4e289d986a4989f75086f6b4cd69b95b57b3
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-02 14:49:01 -0300

    feat: list the states with the most cities

---
+++ module2/desafio2/src/stateList.js
@@ -0,0 +1,44 @@
+"use script";
+import { promises as fs } from "fs";
+
+(async function() {
+    let states = [];
+    let cities = [];
+    let citiesWithUF = [];
+
+    async function init() {
+        try {
+            states = JSON.parse(await fs.readFile('./datas/Estados.json', 'utf-8'));
+            cities = JSON.parse(await fs.readFile('./datas/Cidades.json', 'utf-8'));
+            citiesWithUF = cities.map(city => {
+                return { ...city, UF: states.find(uf => city.Estado === uf.ID).Sigla }
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function listStatesWithTotalCities() {
+        return states.map(state => {
+            const UF = state.Sigla;
+            const totalCities = citiesWithUF.filter(city => city.UF === UF)
+                                        .length;
+            return { 
+                UF,
+                totalCities
+            }
+        }).sort((a, b) => b.totalCities - a.totalCities);
+    }
+
+    function listFiveStatesWithMoresCities() {
+        const countCitiesForStates = listStatesWithTotalCities().slice(0, 5).map(uf => {
+            return `${uf.UF} - ${uf.totalCities}`;
+        });
+        console.log(countCitiesForStates);
+    }
+
+    await init();
+
+    listFiveStatesWithMoresCities();
+
+})();
