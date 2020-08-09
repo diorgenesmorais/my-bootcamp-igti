bccc50f3d38dd36bbec77afdfe2b04a5c5c64753
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-09 16:03:51 -0300

    feat: add account no arquivo

--- module2/bank-api/router/accounts.js
+++ module2/bank-api/router/accounts.js
@@ -1,10 +1,20 @@
 "use strict";
 import express from 'express';
+import { promises as fs } from 'fs';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    res.status(201).send('conta criar');
+router.post('/', async (req, res) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await fs.readFile('accounts.json'));
+        account = { id: data.nextId++, ...account };
+        data.accounts.push(account);
+        await fs.writeFile('accounts.json', JSON.stringify(data, null, 2));
+        res.status(201).send(account);
+    } catch (error) {
+        res.status(404).send({ err: error.message });
+    }
 });
 
 export default router;
