8d4cb8635820122c6e822c7c39132f3cc70fa1a9
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-09 11:34:01 -0300

    feat: gravar os logs com winston

--- module2/middlewares/src/clientRouter.js
+++ module2/middlewares/src/clientRouter.js
@@ -1,11 +1,30 @@
 "use strict";
 import express from "express";
 import client from "./clientes.js";
+import winston from "winston";
 
 const router = express.Router();
+const { combine, printf, label, timestamp } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp}) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+    level: "warn",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: "logs.log" })
+    ],
+    format: combine(
+        label({ label: "Client app"}),
+        timestamp(),
+        myFormat
+    )
+})
 
 function throwErr(err, req, res, next) {
-    console.log('ERROR:', err);
+    logger.warn(err);
     res.status(500).send('Ocorreu um error');
 }
 
