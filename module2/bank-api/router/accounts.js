e8ddbd67325917bc24a45986e5c36830e836c633
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-09 16:28:32 -0300

    refactor: desestruturando o fs

--- module2/bank-api/router/accounts.js
+++ module2/bank-api/router/accounts.js
@@ -2,15 +2,16 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 
+const { readFile, writeFile } = fs;
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
         let account = req.body;
-        const data = JSON.parse(await fs.readFile(global.fileName));
+        const data = JSON.parse(await readFile(global.fileName));
         account = { id: data.nextId++, ...account };
         data.accounts.push(account);
-        await fs.writeFile(global.fileName, JSON.stringify(data, null, 2));
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
         res.status(201).send(account);
     } catch (error) {
         res.status(404).send({ err: error.message });
@@ -19,7 +20,7 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
-        const data = JSON.parse(await fs.readFile(global.fileName));
+        const data = JSON.parse(await readFile(global.fileName));
         delete data.nextId;
         res.send(data);
     } catch (error) {
