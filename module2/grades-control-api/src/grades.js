24cfa6fbef9b1a4aa9c06e8b5177ec414d5ca8a0
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-09 19:19:19 -0300

    feat: criar uma grade com método post

--- module2/grades-control-api/src/grades.js
+++ module2/grades-control-api/src/grades.js
@@ -1,10 +1,23 @@
 "use strict";
 import express from 'express';
+import { promises as fs, read } from 'fs';
+const { readFile, writeFile } = fs;
+const fileName = 'data/grades.json';
 
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-    next(new Error('Teste erro'));
+    try {
+        let grade = req.body;
+        const datas = JSON.parse(await readFile(fileName));
+        const timestamp = new Date();
+        grade = { id: datas.nextId++, ...grade, timestamp };
+        datas.grades.push(grade);
+        await writeFile(fileName, JSON.stringify(datas, null, 2));
+        res.send(datas);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.use((err, req, res, next) => {
