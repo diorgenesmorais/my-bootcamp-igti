dd338466f05aafc22f54b4495a21827c8d2387cb
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-16 15:29:10 -0300

    feat: add buttons and interpolation

--- module3/react-counter-01/src/components/Counter/Counter.js
+++ module3/react-counter-01/src/components/Counter/Counter.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+    constructor() {
+        super();
+        this.currentCounter = 2;
+    }
+
     render() {
         return (
             <div>
-                Counter
+                <button className="waves-effect waves-light btn red darken-4">-</button>
+                <span>{ this.currentCounter }</span>
+                <button className="waves-effect waves-light btn green darken-4">+</button>
             </div>
         )
     }
