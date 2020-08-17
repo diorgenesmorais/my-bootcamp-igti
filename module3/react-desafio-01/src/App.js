107b14c9c61438d6ba1aab3aa25c681c84e97ff2
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-17 11:11:01 -0300

    feat: definir os inputs readOnly

--- module3/react-desafio-01/src/App.js
+++ module3/react-desafio-01/src/App.js
@@ -1,29 +1,96 @@
 import React, { Component } from 'react';
 import Bar from './components/Bar/Bar';
-import Input from './components/Input/Input';
+// import Input from './components/Input/Input';
+
+import { calculateSalaryFrom } from './helpers/salary';
 
 export default class App extends Component {
   constructor() {
     super();
 
+    this.numberFormat = Intl.NumberFormat('pt-BR', { currency: 'BRL', minimumIntegerDigits: 1, minimumFractionDigits: 2 });
     this.state = {
-      base: 0,
-      inss: 0,
-      irpf: 0,
-      liquido: 100
+      salary: 0,
+      baseInss: 0,
+      descontoInss: 0,
+      baseIrpf: 0,
+      descontoIrpf: 0,
+      liquido: 925
     };
   }
 
+  handlerInputValue = (event) => {
+    const { value } = event.target;
+
+    if (value) {
+      const salary = parseFloat(value);
+      this.setState({salary});
+    }
+  }
+
+  format = (value) => {
+    return `R$ ${this.numberFormat.format(value)}`;
+  }
+
+  porcentagemLiquido = (value) => {
+    const { salary } = this.state;
+    return (value / salary) * 100;
+  }
+
+  valorComPorcentagem = (value = 0) => {
+    const result = this.format(value);
+    return `${result} (${this.porcentagemLiquido(value)}%)`;
+  }
+
+  componentDidUpdate(_, previousStates) {
+    const { salary } = this.state;
+    if (previousStates.salary !== salary) {
+      this.setState({
+        baseInss: calculateSalaryFrom(salary).baseINSS,
+        descontoInss: calculateSalaryFrom(salary).discountINSS,
+        baseIrpf: calculateSalaryFrom(salary).baseIRPF,
+        descontoIrpf: calculateSalaryFrom(salary).discountIRPF,
+        liquido: calculateSalaryFrom(salary).netSalary
+      });
+    }
+  }
+
   render() {
-    const { base, inss, irpf, liquido } = this.state;
+    const { baseInss, descontoInss, baseIrpf, descontoIrpf, liquido } = this.state;
+
     return (
       <div className="container">
+        <h1 className="center-align">React Salário</h1>
         <div className="row">
             <form className="col s12">
               <div className="row">
                 <div className="input-field col s12">
-                  <Input id="myInput" />
-                  <label for="myInput">Salário Bruto</label>
+                  <input id="myInput" type="number" onChange={this.handlerInputValue} />
+                  <label htmlFor="myInput">Salário Bruto</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s3">
+                  <input type="text" value={this.format(baseInss)} readOnly />
+                  <label>Base INSS</label>
+                </div>
+                <div className="input-field col s3">
+                  <input type="text" value={descontoInss} readOnly />
+                  <label>Desconto INSS</label>
+                </div>
+                <div className="input-field col s3">
+                  <input type="text" value={baseIrpf} readOnly />
+                  <label>Base IRPF</label>
+                </div>
+                <div className="input-field col s3">
+                  <input type="text" value={descontoIrpf} readOnly />
+                  <label>Desconto IRPF</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s3">
+                  <input type="text" value={liquido} readOnly />
+                  <label>Salário líquido</label>
                 </div>
               </div>
             </form>
@@ -36,9 +103,9 @@ export default class App extends Component {
               alignItems: "center",
               justifyContent: "center"
             }}>
-              <Bar value={inss} color={'orange'} />
-              <Bar value={irpf} color={'red'} />
-              <Bar value={liquido} color={'green'} />
+              <Bar value={this.porcentagemLiquido(descontoInss)} color={'orange'} />
+              <Bar value={this.porcentagemLiquido(descontoIrpf)} color={'red'} />
+              <Bar value={this.porcentagemLiquido(liquido)} color={'green'} />
             </div>
           </div>
         </div>
