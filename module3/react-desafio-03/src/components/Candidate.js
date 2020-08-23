c4bae72597de17ad37fa12c021a970891e6096a7
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-23 08:18:20 -0300

    feat: criar candidate module css

--- module3/react-desafio-03/src/components/Candidate.js
+++ module3/react-desafio-03/src/components/Candidate.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Position from './Position';
 import Picture from './Picture';
+import css from './candidate.module.css';
 
 export default function Candidate({ candidate, position }) {
     const { id, name, votes } = candidate;
     const imageSource = `img/${id}.jpg`;
     return (
-        <>
+        <div className={css.candidate}>
             <Position>{ position }</Position>
             <Picture imageSource={imageSource} description={name} />
             <div>
                 { name } - { votes }
             </div>
-        </>
+        </div>
     )
 }
