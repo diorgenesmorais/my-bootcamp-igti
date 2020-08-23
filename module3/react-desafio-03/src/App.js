0559f70361ae562f622f754b395261319e0352c0
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-22 21:42:03 -0300

    feat: criar state

--- module3/react-desafio-03/src/App.js
+++ module3/react-desafio-03/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      candidates: []
+    }
+  }
+
   render() {
     return <h1>Desafio 03</h1>;
   }
