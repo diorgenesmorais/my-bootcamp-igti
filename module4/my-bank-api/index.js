43a3ad3f582faff6223ba2a685f5b141d447e2fa
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-30 20:19:52 -0300

    feat(tp/module4): create connect mongo atlas

--- module4/my-bank-api/index.js
+++ module4/my-bank-api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect('mongodb+srv://chaveirinho:mi@Rapariga@cluster0.e2zaw.mongodb.net/grades?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conectado ao MongoDB com sucess!');
+    } catch (error) {
+        console.log('Erro ao conectar no MongoDB', error);
+    }
+})();
 
 app.use(express.json());
 
