c09f94c7d836903d1f1a423062992c717f3b7025
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-08-30 17:31:09 -0300

    feat(api/mongoose): create mongo atlas connect

--- module4/connect-mongoose/app.js
+++ module4/connect-mongoose/app.js
@@ -1,5 +1,18 @@
 import express from 'express';
 import { studentRouter } from './routes/student.router.js';
+import mongoose from 'mongoose';
+
+(async () => {
+    try {
+        await mongoose.connect('mongodb+srv://chaveirinho:mi@Rapariga@cluster0.e2zaw.mongodb.net/grades?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conectado ao MongoDB com sucess!');
+    } catch (error) {
+        console.log('Erro ao conectar no MongoDB', error);
+    }
+})();
 
 const app = express();
 
--- module4/connect-mongoose/routes/student.router.js
+++ module4/connect-mongoose/routes/student.router.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import { studentModel } from '../models/student.model.js';
 
 const app = express();
 
-
+app.post('/student', async (req, res) => {
+    try {
+        const student = new studentModel(req.body);
+        student.save();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
 
 export { app as studentRouter };
